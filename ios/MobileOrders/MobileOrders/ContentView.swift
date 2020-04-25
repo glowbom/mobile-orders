9e667eaaf57decd943119ec5aaa2655af8328653
Jacob Ilin <me@jacobilin.com>
2020-04-25 03:05:05 -0600

    Mobile Orders for iOS project created

---
+++ ios/MobileOrders/MobileOrders/ContentView.swift
@@ -0,0 +1,42 @@
+//
+//  ContentView.swift
+//  MobileOrders
+//
+//  Created by Jacob Ilin on 4/25/20.
+//  Copyright © 2020 Glowbom. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var selection = 0
+ 
+    var body: some View {
+        TabView(selection: $selection){
+            Text("First View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("first")
+                        Text("First")
+                    }
+                }
+                .tag(0)
+            Text("Second View")
+                .font(.title)
+                .tabItem {
+                    VStack {
+                        Image("second")
+                        Text("Second")
+                    }
+                }
+                .tag(1)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
